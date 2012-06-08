eaaa81a907d07f415d16b48ef7d34ed6dc2675fc
sgreenman <shawn.greenman@gmail.com>
2012-06-08 07:39:12 -0700

    project creation

--- src/com/aremote/MainActivity.java
+++ src/com/aremote/MainActivity.java
@@ -2,14 +2,25 @@ package com.aremote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import com.aremote.com.aremote.drawing.DrawView;
 
 public class MainActivity extends Activity
 {
+   DrawView drawing;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       super.onCreate(savedInstanceState);
+       //setContentView(R.layout.main);
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       requestWindowFeature(Window.FEATURE_NO_TITLE);
+       drawing = new DrawView(this);
+       setContentView(drawing);
+       drawing.requestFocus();
     }
 }
